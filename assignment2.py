fb1cc461065e5b010973c4174e32e13b811f040d
Thit Sann Oo <58143685+tsoo2-seneca@users.noreply.github.com>
2025-07-23 23:28:55 -0400

    byte to read

---
+++ assignment2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def human_readable_size(size_bytes):
+    # Convert bytes to a human-readable format
+    for unit in ['B','KB','MB','GB','TB']:
+        if size_bytes < 1024.0:
+            return f"{size_bytes:.2f} {unit}"
+        size_bytes /= 1024.0
+    return f"{size_bytes:.2f} PB"
+
