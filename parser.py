ed5b631a2acf93da9f854d0db6454c55eb5c26ff
Ricky Tang <rtang41@myseneca.ca>
2025-07-28 10:06:22 -0400

    cleaned up script

--- parser.py
+++ parser.py
@@ -2,31 +2,31 @@
 # Author: Ricky Tang
 # ID: rtang41
 # Student No. : 104448246
-"""
-script for argument parsing command for directory analyzer tool
-"""
 
 import argparse
 
-# messing around with argparse to start/practice
-# script to echo the argument parsed
-# parser = argparse.ArgumentParser()
-# parser.add_argument("echo")
-# args = parser.parse_args()
-# print(args.echo)
-
 def parse_args():
+    """
+    Function for parsing input arguments for analyzer tool
+    """
     parser = argparse.ArgumentParser(description = "Directory analyzer tool")
+    #dir path argument
     parser.add_argument(
         "directory", type = str,
         help = "path to directory to analyze")
+    #largest files argument
     parser.add_argument(
         "--top", "-t", type = int, default = 0,
         help = "Show top N largest files")
+    #filetype sort argument
     parser.add_argument(
-        "--sort", "-s", type = str,
+        "--sort", "-s", type = str, default = "name",
         help = "sort by <size>, <name>, or <filetype>"
+    )
+    #write output file argument
     parser.add_argument(
         "--output", "-o", type = str,
         help = "path to output file"
     )
+
+    return parser.parse_args()
