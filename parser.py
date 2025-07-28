f6e904bf3900439ff283e49c9f19f08fa2e84da3
Ricky Tang <rtang41@myseneca.ca>
2025-07-28 10:28:51 -0400

    commit for feedback/testing

--- parser.py
+++ parser.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 # Author: Ricky Tang
 # ID: rtang41
-# Student No. : 104448246
+# Student No.: 104448246
 
 import argparse
 
 def parse_args():
     """
-    Function for parsing input arguments for analyzer tool
+    Function for parsing input arguments for analyzer tool with argparse
     """
     parser = argparse.ArgumentParser(description = "Directory analyzer tool")
     #dir path argument
     parser.add_argument(
         "directory", type = str,
-        help = "path to directory to analyze")
+        help = "path to directory to analyze"
+    )
     #largest files argument
     parser.add_argument(
         "--top", "-t", type = int, default = 0,
-        help = "Show top N largest files")
+        help = "Show top N largest files"
+    )
     #filetype sort argument
     parser.add_argument(
         "--sort", "-s", type = str, default = "name",
-        help = "sort by <size>, <name>, or <filetype>"
+        help = "sort by <size>|<name>|<filetype>"
     )
     #write output file argument
     parser.add_argument(
