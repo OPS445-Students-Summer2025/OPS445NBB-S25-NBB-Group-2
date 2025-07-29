f9096cc738b707430d1ffe1e95093c715dcff5e8
Mark Jordan Ndolla Wafo <mjndolla-wafo@myseneca.ca>
2025-07-29 12:26:40 -0400

    Add function header and docstring for count_files_and_folders

---
+++ a2.py
@@ -0,0 +1,13 @@
+import os
+
+def count_files_and_folders(directory):
+    """
+    Count the number of files and folders inside the given directory recursively.
+
+    Args:
+        directory (str): The path to the directory to scan.
+
+    Returns:
+        tuple: (number_of_files, number_of_folders)
+    """
+    pass  # Logic will be added later
