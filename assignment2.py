b10ef633286d1ed5fdd98313286cf4d0699b6f2c
Ricky Tang <rtang41@myseneca.ca>
2025-08-02 15:21:54 -0400

    final edits from parser.py

---
+++ assignment2.py
@@ -0,0 +1,36 @@
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser(
+    description = "Directory size report tool"
+)
+
+#dir path argument
+parser.add_argument(
+    "--dir",
+    required = True,
+    help = "Directory path to scan"
+)
+
+#largest files argument
+parser.add_argument(
+    "--top", "-t",
+    type = int,
+    help = "Show top N largest files"
+)
+
+#filetype sort argument
+parser.add_argument(
+    "--sort", "-s",
+    choices = ["size","name","type"],
+    help = "sort by <size>|<name>|<type>"
+)
+
+#write output file argument
+parser.add_argument(
+    "--output", "-o",
+    help = "path to output file (optional)"
+)
+
+args = parser.parse_args()
