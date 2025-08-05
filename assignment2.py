504bd73b62622182a876d2a9f74c89e49cbb4bb3
Kris Sadiasa <kalmodal@myseneca.ca>
2025-08-04 20:15:01 -0400

    simplified codes

--- assignment2.py
+++ assignment2.py
@@ -1,21 +1,18 @@
 def get_total_size(path):
-    total_size = 0
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            fp = os.path.join(root, f)
-            if os.path.isfile(fp):
-                total_size += os.path.getsize(fp)
-    return total_size
+    return sum(
+        os.path.getsize(os.path.join(root, f))
+        for root, _, files in os.walk(path)
+        for f in files
+        if os.path.isfile(os.path.join(root, f))
+    )
 
 def get_top_n_files(path, n):
-    all_files = []
-    for root, dirs, files in os.walk(path):
+    files_with_size = []
+    for root, _, files in os.walk(path):
         for f in files:
             fp = os.path.join(root, f)
             try:
-                size = os.path.getsize(fp)
-                all_files.append((fp, size))
-            except:
+                files_with_size.append((fp, os.path.getsize(fp)))
+            except (OSError, PermissionError):
                 continue
-    all_files.sort(key=lambda x: x[1], reverse=True)
-    return all_files[:n]
+    return sorted(files_with_size, key=lambda x: x[1], reverse=True)[:n]
